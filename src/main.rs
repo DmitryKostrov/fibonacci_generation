9bec114b489b5fe142bfa9606cdf35699bda446d
DmitryKostrov <dimon3limon3@gmail.com>
2024-01-30 17:43:46 +0300

    exercise_completed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+
+    println!("Enter Fibonacci number"); // Сообщаем пользователю что нужно ввести n-ое число фибоначчи.
+    let mut input_user: String = String::new(); // создаем переменную для ввода пользователя
+    io::stdin() // просим ввести что нибудь
+        .read_line(&mut input_user)// записываем номер числа пользователя
+        .expect("Failed to read line"); // в случае ошибки ввода выводим сообщение в консоль
+    let input_user: i32 = input_user.trim().parse().expect("\n!!!Enter a positive number!!!\n\n");// конвертируем строку в число
+
+    println!("\n"); // Создаем пустое место в консоли чтобы ввод пользователя не смешалось с результатом выполнения программы
+    // проверяем ввод пользователя
+
+    if input_user >= 1 { // Проверка числа положительное оно или нет
+
+        println!("0");
+        println!("1");
+
+        let mut fibo_one: u64 = 0;
+        let mut fibo_two: u64= 1;
+        for _num in 1..input_user {
+
+            let next: u64 = fibo_one + fibo_two; // следующее значение
+            fibo_one = fibo_two;
+            fibo_two = next;
+            println!("{}", fibo_two);
+        }
+    }
+    else { // если число отрицателььное выводим 0 с вопросом
+        println!("0");
+        println!("Are you sure you entered a positive number?");
+    }
 }
