1b2d642e1e400d3a9310e41e2e2f0f0c5db271c2
DmitryKostrov <dimon3limon3@gmail.com>
2024-01-30 04:15:16 +0300

    In_developing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
+
+// Генерирование n-го числа Фибоначчи.
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946,….   ;)
 fn main() {
-    println!("Hello, world!");
+    println!("Enter Fibonacci number");
+    let mut input_user: String = String::new();
+    io::stdin()
+        .read_line(&mut input_user)
+        .expect("Failed to read line");
+    let mut input_user: u16 = input_user.trim().parse().expect("Please type a number!\n");
+
+    let mut _fibo1 = 0;
+    let mut _fibo2 = 1;
+    for num in 0..input_user {
+
+        let next: u32 = _fibo1 + _fibo2;
+        _fibo1 = _fibo2;
+        _fibo2 = next;
+        println!("{}", _fibo2);
+    }
 }
